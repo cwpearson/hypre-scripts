1714b80cb8927b4bea430c85e0e1aba9bf8df8ef
Mert Hidayetoglu <hidayet2@illinois.edu>
2021-04-23 18:10:52 -0400

    modify configure script

--- summit-configure-make-install.sh
+++ summit-configure-make-install.sh
@@ -12,8 +12,8 @@ set -x
 #first, clone into this directory
 # git clone git@github.com:hypre-space/hypre.git
 # git checkout v2.19.0 
-HYPRE_DIR=$HOME/repos/hypre
-INSTALL_DIR=$HOME/hypre
+HYPRE_DIR=$HOME/AMG/hypre
+INSTALL_DIR=$HOME/AMG
 
 cd $HYPRE_DIR
 
@@ -31,6 +31,7 @@ cd $HYPRE_DIR/src
   --with-MPI \
   --enable-gpu-aware-mpi \
   --enable-shared \
+  --enable-unified-memory \
   CC=mpicc CXX=mpicxx
 
 nice -n20 make -j `nproc` install
